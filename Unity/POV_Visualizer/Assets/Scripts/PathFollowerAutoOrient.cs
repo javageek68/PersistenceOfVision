b662ac9e204f57388f5a7224aff548f69d4a4b2a
James Michael Brascome <Brascome@uab.edu>
2024-08-09 19:37:37 -0500

    tweaked the terrain, added the fed alpha as the hero vehicle, and added a follower script

---
+++ Unity/POV_Visualizer/Assets/Scripts/PathFollowerAutoOrient.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+namespace PathCreation.Examples
+{
+    // Moves along a path at constant speed.
+    // Depending on the end of path instruction, will either loop, reverse, or stop at the end of the path.
+    public class PathFollowerAutoOrient : MonoBehaviour
+    {
+        public PathCreator pathCreator;
+        public EndOfPathInstruction endOfPathInstruction;
+        public float speed = 5;
+        public float lookAhead;
+        float distanceTravelled;
+        
+
+        void Start() {
+            if (pathCreator != null)
+            {
+                // Subscribed to the pathUpdated event so that we're notified if the path changes during the game
+                pathCreator.pathUpdated += OnPathChanged;
+            }
+        }
+
+        void Update()
+        {
+            if (pathCreator != null)
+            {
+                distanceTravelled += speed * Time.deltaTime;
+                transform.position = pathCreator.path.GetPointAtDistance(distanceTravelled, endOfPathInstruction);
+                Vector3 pointAhead = pathCreator.path.GetPointAtDistance(distanceTravelled+lookAhead, endOfPathInstruction);
+                //transform.rotation = pathCreator.path.GetRotationAtDistance(distanceTravelled, endOfPathInstruction);
+                transform.LookAt(pointAhead, Vector3.up);
+            }
+        }
+
+        // If the path changes during the game, update the distance travelled so that the follower's position on the new path
+        // is as close as possible to its position on the old path
+        void OnPathChanged() {
+            distanceTravelled = pathCreator.path.GetClosestDistanceAlongPath(transform.position);
+        }
+    }
+}
